a9f3f402edf2b5440af5eb9f104e918b575a1ba4
lilydove <wuyiwangyi@163.com>
2017-05-16 21:59:08 +0800

    2017-05-16 增加浏览器icon

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
+
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=gb2312" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no" />
 <meta name="format-detection" content="telephone=no"/>
+<link rel="Shortcut Icon" href="img/mainicon.jpg" type="image/x-icon">
 <title>合肥环保科技</title>
 </head>
 
